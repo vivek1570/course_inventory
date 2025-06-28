2906d34dae53045690167225ac2656de2d3897df
vivek1570 <kpvivekvettam@gmail.com>
2025-06-28 14:48:29 +0530

    feat:added enrollment for students

--- JavaMysql_1.java
+++ JavaMysql_1.java
@@ -53,27 +53,6 @@ public class JavaMysql_1 {
             System.out.println("Error in inserting course: "+e.getMessage());
         }
     }
-    // public static void enroll_student_to_course(Connection conn)
-    // {
-    //     /*
-    //      * so first ask which student want to enroll,
-    //      * get a name as input 
-    //      * if name does'nt exists then try another person, 
-    //      * if exists it saves as variable
-    //      * then ask which course want to enroll same as above we do it
-    //      * then add into it
-    //      */
-    //     try{
-    //         Scanner sc=new Scanner(System.in);
-    //         System.out.println("Enter the student name:");
-    //         String name=sc.nextLine();
-    //         System.out.println("Enter the course name:");
-    //         String course=sc.nextLine();
-
-    //         String sql="insert into "
-
-    //     }
-    // }
     public static void view_all_students(Connection conn)
     {
         try{
@@ -118,6 +97,92 @@ public class JavaMysql_1 {
             System.out.println("Error in listing course: "+e.getMessage());
         }
     }
+    public static void enroll_student_to_course(Connection conn)
+    {
+        /*
+         * so first ask which student want to enroll,
+         * get a name as input 
+         * if name does'nt exists then try another person, 
+         * if exists it saves as variable
+         * then ask which course want to enroll same as above we do it
+         * then add into it
+         */
+        try{
+            /*
+             * student data fetch 
+             */
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the student name:");
+            String name=sc.nextLine();
+            String sql="select * from students";
+            Statement stmt=conn.createStatement();
+            ResultSet rs=stmt.executeQuery(sql);
+            int s_id=0;
+            int x=0;
+            while(rs.next())
+            {
+                x=x+1;
+                String out=rs.getString("name");
+                if(out.equals(name))
+                {
+                    s_id=x;
+                }
+            }
+            if(s_id==0)
+            {
+                System.out.println("Name "+name+" not found in students db");
+                sc.close();
+                return;
+            }
+            else{
+                System.out.println("Name "+name+" found in db with id "+s_id);
+            }
+            /*
+             * course data fetch
+             */
+            System.out.println("Enter the course name:");
+            String course=sc.nextLine();
+            sql="select * from courses";
+            // stmt=conn.createStatement();
+            rs=stmt.executeQuery(sql);
+            int c_id=0;
+            x=0;
+            while(rs.next())
+            {
+                x=x+1;
+                String out=rs.getString("title");
+                if(out.equals(course))
+                c_id=x;
+            }
+            if(c_id==0)
+            {
+                System.out.println("Course "+course+" not found in db");
+                sc.close();
+                return;
+            }
+            else{
+                System.out.println("Course "+course+" found in db with id "+c_id);
+            }
+
+            sql="insert into enrollments(student_id,course_id) values(?,?)";
+            PreparedStatement stmt_p=conn.prepareStatement(sql);
+            stmt_p.setInt(1,s_id);
+            stmt_p.setInt(2,c_id);
+            int rows=stmt_p.executeUpdate();
+            if(rows>0)
+            {
+                System.out.println("enrollment done successfully");
+            }
+            else{
+                System.out.println("fails to do enrollment");
+            }
+            sc.close();
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Error in enrollment of student and course: "+e.getMessage());
+        }
+    }
     public static void main(String[] args)
     {
         String url = "jdbc:mysql://localhost:3306/student_db";
@@ -131,9 +196,9 @@ public class JavaMysql_1 {
                 "📋 Main Menu\n" +
                 "1. Add Student\n" +
                 "2. Add Course\n" +
-                "3. Enroll Student in Course\n" +
                 "4. View All Students\n" +
                 "5. View All Courses\n" +
+                "3. Enroll Student in Course\n" +
                 "6. View All Enrollments\n"+
                 "7. Exit"
             );
@@ -149,9 +214,9 @@ public class JavaMysql_1 {
                 case 2:
                     add_course(conn);
                     break;
-                // case 3:
-                //     enroll_student_to_course(conn);
-                //     break;
+                case 3:
+                    enroll_student_to_course(conn);
+                    break;
                 case 4:
                     view_all_students(conn);
                     break;
